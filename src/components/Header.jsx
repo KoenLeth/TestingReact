14b375f55410618210191d429f5ff147a6f96d51
KoenLeth <lefonix@gmail.com>
2021-02-17 12:47:56 +0300

    TheFirstCommit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src='https://download.logo.wine/logo/Disney%2B/Disney%2B-Logo.wine.png'></img>
+        </header>
+    );
+}
+
+export default Header;
